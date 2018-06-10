e4da358337d8bd0a8a68d040278c7b58f467acd9
dosdarwin <s103395@cmes.tn.edu.tw>
2018-06-10 11:38:00 +0800

    New version

--- driving.py
+++ driving.py
@@ -1,12 +1,15 @@
-country = input('What country are you from(type in Taiwan or America):') 
-age = int(input('how old are you:'))
-if country == 'Taiwan':
-	if age >= 18:
-		print("you can have drivers' license")
-	else:
-		print("you cannot have drivers' license")
-if country == 'America':
-	if age >= 16:
-		print("you can have drivers' license")
-	else:
-		print("you cannot have drivers' license")
+country = input('What country are you from:') 
+if country == 'America' or country == 'Taiwan':
+	age = int(input('how old are you:'))
+	if country == 'Taiwan':
+		if age >= 18:
+			print("you can have drivers' license")
+		else:
+			print("you cannot have drivers' license")
+	elif country == 'America':
+		if age >= 16:
+			print("you can have drivers' license")
+		else:
+			print("you cannot have drivers' license")
+else: 
+	print('please type in Taiwan or America')
